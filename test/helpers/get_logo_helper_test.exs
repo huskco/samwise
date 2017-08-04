8d1ffdd19e1b765b607b686debfd278634df39ec
[Joshua Ogle] <[joshua@joshuaogle.com]>
2017-08-03 18:06:06 -0600

    Better logo from clearbit

--- test/helpers/get_logo_helper_test.exs
+++ test/helpers/get_logo_helper_test.exs
@@ -2,7 +2,7 @@ defmodule Samwise.GetLogoHelperTest do
   use Samwise.ConnCase
 
   test "gets the initial of a word" do
-    assert Samwise.GetLogo.get_logo("netflix.com") == "https://logo.clearbit.com/netflix.com?s=128"
+    assert Samwise.GetLogo.get_logo("netflix.com") == "https://logo.clearbit.com/netflix.com"
   end
 
   test "doesn't try with nil" do
--- web/helpers/get_logo.ex
+++ web/helpers/get_logo.ex
@@ -1,7 +1,7 @@
 defmodule Samwise.GetLogo do
   def get_logo(url) do
     if url do
-      "https://logo.clearbit.com/#{url}?s=128"
+      "https://logo.clearbit.com/#{url}"
     end
   end
 end
