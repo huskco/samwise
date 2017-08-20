6c098e15f14f0d2d57d60c625f013f6ed77e0f6c
Joshua Ogle <joshua@joshuaogle.com>
2017-08-19 20:20:17 -0600

    Don't cache manifest

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :samwise, Samwise.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "http", host: "sam.husk.co", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  #cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
