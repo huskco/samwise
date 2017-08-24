a78eb1fd9d6912699ad3a7c42a61a1ae8f31bf92
Joshua Ogle <joshua@joshuaogle.com>
2017-08-24 12:46:47 -0600

    Make Slack work better with tests

--- lib/samwise/slack.ex
+++ lib/samwise/slack.ex
@@ -1,19 +1,24 @@
 defmodule Samwise.Slack do
+  @moduledoc """
+  A helpful bot for Ogles
+  """
   use Slack
 
   alias Samwise.Money.BankAccountController
   alias Samwise.Money.ForecastController
+  alias Number.Currency
 
   def handle_connect(slack, state) do
     IO.puts "Connected to Slack as #{slack.me.name}"
     {:ok, state}
   end
 
-  def handle_event(message = %{type: "message"}, slack, state) do
+  def handle_event(%{type: "message"} = message, slack, state) do
     slacker_name = lookup_user_name(message.user, slack)
     if slacker_name != ("@" <> slack.me.name) do
       IO.puts "Received Slack message: #{message.text} from #{slacker_name}"
-      match_commands(message.text, slacker_name)
+      message.text
+        |> match_commands(slacker_name)
         |> send_message(message.channel, slack)
     end
 
@@ -30,11 +35,11 @@ defmodule Samwise.Slack do
   end
   def handle_info(_, _, state), do: {:ok, state}
 
-  def match_commands(text, slacker_name) do
+  def match_commands(text, _slacker_name) do
     case text do
       "What is my balance" -> handle_balance()
       "How much can I spend" -> handle_available_to_spend()
-      #_ -> "Sorry #{slacker_name}, I didn't understand that"
+      # _ -> "Sorry #{slacker_name}, I didn't understand that"
     end
   end
 
@@ -42,17 +47,17 @@ defmodule Samwise.Slack do
 
   def handle_balance do
     balance = BankAccountController.balance()
-      |> Number.Currency.number_to_currency
+      |> Currency.number_to_currency
 
     savings = BankAccountController.savings()
-      |> Number.Currency.number_to_currency
+      |> Currency.number_to_currency
 
     "Your balance is #{balance}, with #{savings} in savings"
   end
 
   def handle_available_to_spend do
     available_to_spend = ForecastController.get_available_to_spend()
-      |> Number.Currency.number_to_currency
+      |> Currency.number_to_currency
 
     "You have #{available_to_spend} available"
   end
